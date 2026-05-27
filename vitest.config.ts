f79d4a491bee758d104c4d7996329c416a92f2dd
Eli <88557639+lishaduck@users.noreply.github.com>
2026-05-27 08:53:14 -0500

    ci: e2e matrix (#2754)
    <!-- 👋 Hi, thanks for sending a PR to flint! ❤️‍🔥
    Please fill out all fields below and make sure each item is true and [x]
    checked.
    Otherwise we may not be able to review your PR. -->
    
    ## PR Checklist
    
    - [ ] Addresses an existing open issue: fixes #000
    - [ ] That issue was marked as [`status: accepting
    prs`](https://github.com/flint-fyi/flint/issues?q=is%3Aopen+is%3Aissue+label%3A%22status%3A+accepting+prs%22)
    - [ ] Steps in
    [CONTRIBUTING.md](https://github.com/flint-fyi/flint/blob/main/.github/CONTRIBUTING.md)
    were taken
    
    ## Overview
    
    <!-- Description of what is changed and how the code change does that.
    -->
    Ooops. Thought I'd done this in #2429.
    
    ---------
    
    Co-authored-by: Josh Goldberg <git@joshuakgoldberg.com>

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,5 +1,6 @@
 import { readdirSync } from "node:fs";
 import path from "node:path";
+import { platform } from "node:process";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
@@ -19,7 +20,8 @@ export default defineConfig({
 						"@flint.fyi/ts-patch/install-patch-hooks",
 					],
 					snapshotSerializers: name === "e2e" ? ["vitest-ansi-serializer"] : [],
-					testTimeout: name === "e2e" ? 20_000 : 10_000,
+					testTimeout:
+						name === "e2e" ? (platform === "win32" ? 60_000 : 20_000) : 10_000,
 				},
 			}),
 		),

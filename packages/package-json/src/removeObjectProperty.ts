33056b219e66701a6c7ce27e5b29eed2dbc3e259
michael faith <michaelfaith@users.noreply.github.com>
2026-06-08 17:22:03 -0500

    refactor(package-json): migrate `privatePresence`, `publishConfigRedundancy`, `repositoryShorthand`, and `scriptsNameCasing` to new language (#2913)
    <!-- 👋 Hi, thanks for sending a PR to flint! ❤️‍🔥
    Please fill out all fields below and make sure each item is true and [x]
    checked.
    Otherwise we may not be able to review your PR. -->
    
    ## PR Checklist
    
    - [x] Addresses an existing open issue: 
       - fixes #2881
       - fixes #2882
       - fixes #2883
       - fixes #2884
    - [x] That issue was marked as [`status: accepting
    prs`](https://github.com/flint-fyi/flint/issues?q=is%3Aopen+is%3Aissue+label%3A%22status%3A+accepting+prs%22)
    - [x] Steps in
    [CONTRIBUTING.md](https://github.com/flint-fyi/flint/blob/main/.github/CONTRIBUTING.md)
    were taken
    
    ## Overview
    
    <!-- Description of what is changed and how the code change does that.
    -->

--- packages/package-json/src/removeObjectProperty.ts
+++ packages/package-json/src/removeObjectProperty.ts
@@ -1,11 +1,6 @@
 import type { MemberNode, ObjectNode } from "@humanwhocodes/momoa";
 
-import {
-	getJsonNodeRange as getJsonNodeRangeLegacy,
-	type JsonSourceFile,
-} from "@flint.fyi/json-language";
 import { getJsonNodeRange } from "@flint.fyi/json-language/new";
-import type { AST } from "@flint.fyi/typescript-language";
 
 export function removeObjectProperty(
 	propertyNode: MemberNode,
@@ -48,52 +43,6 @@ export function removeObjectProperty(
 			range: {
 				begin: previousEnd,
 				end: propertyEnd,
-			},
-			text: "",
-		};
-	}
-
-	throw new Error("Expected object property to have a sibling.");
-}
-
-export function removeObjectPropertyLegacy(
-	sourceFile: JsonSourceFile,
-	property: AST.PropertyAssignment,
-	objectNode: AST.ObjectLiteralExpression,
-) {
-	if (objectNode.properties.length === 1) {
-		return {
-			range: getJsonNodeRangeLegacy(objectNode, sourceFile),
-			text: "{}",
-		};
-	}
-
-	const index = objectNode.properties.indexOf(property);
-	if (index === -1) {
-		throw new Error("Node is not a child of the parent object.");
-	}
-
-	const previous = index > 0 ? objectNode.properties[index - 1] : undefined;
-	const next =
-		index < objectNode.properties.length - 1
-			? objectNode.properties[index + 1]
-			: undefined;
-
-	if (next) {
-		return {
-			range: {
-				begin: property.getStart(sourceFile),
-				end: next.getStart(sourceFile),
-			},
-			text: "",
-		};
-	}
-
-	if (previous) {
-		return {
-			range: {
-				begin: previous.end,
-				end: property.end,
 			},
 			text: "",
 		};
